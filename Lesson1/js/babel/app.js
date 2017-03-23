8d67abe3c59910e9636cf211db6aea7d1b6257ec
AlexandraOlegovna <nikolenko.rnd@gmail.com>
2017-03-23 23:50:33 +0300

    make left menu #1

--- Lesson1/js/babel/app.js
+++ Lesson1/js/babel/app.js
@@ -1,7 +1,26 @@
 "use strict";
 
-var x = "123";
+var $ = function $(s) {
+    return document.querySelector(s);
+};
+var a = $("#hamburger");
+var isOpen = false;
+var content = $(".all_content");
+
+a.onclick = function (_) {
+    var menu = $(".title_navbar");
+
+    if (!isOpen) {
+        menu.style.display = "block";
+        // content.style.transform = "translate3d(273px, 0px, 0px)"
+        isOpen = true;
+    } else {
+        menu.style.display = "";
+        // content.style.transform = "translate3d(0px, 0px, 0px)"
+        isOpen = false;
+    }
+};
 
 console.log([1, 2, 3].map(function (n) {
-  return Math.pow(n, 2);
+    return Math.pow(n, 2);
 }));
